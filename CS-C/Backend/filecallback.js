285e2f5212003e593eac1a474680415ad64e5b1f
kunikapremi1108 <kunikapremi11@gmail.com>
2025-02-07 12:45:27 +0530

    completed

---
+++ CS-C/Backend/filecallback.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const read = () =>{
+    fs.readFile("./data.txt","utf-8",(error, data) =>{
+        if (error) {
+            console.error("Error reading file:", error);
+
+        }
+        else{
+            console.log("File content:", data);
+        }
+
+    });
+}
+//read();
+//console.log("first")
+module.exports = read;
---
+++ CS-C/Backend/fileimport.js
@@ -0,0 +1,7 @@
+const read = require ("./filecallback.js");
+
+read();
+console.log("first")
+console.log("second")
+console.log("3rd")
+console.log("4th")
