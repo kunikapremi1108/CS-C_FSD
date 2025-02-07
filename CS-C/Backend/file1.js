0baf0f54fa4dae46d217573db72f546a704964ff
kunikapremi1108 <kunikapremi11@gmail.com>
2025-02-07 12:25:00 +0530

    updated on 07/02/2025

--- CS-C/Backend/file1.js
+++ CS-C/Backend/file1.js
@@ -1,3 +1,13 @@
 const fs = require("fs");
 const data = fs.readFileSync("./data.txt", "utf8");
 console.log(data);
+if(data.match("H")){
+    console.log("H is present")
+    const newdata = data.replace("H","AB")
+    fs.writeFileSync("./data.txt",newdata,"utf8");
+}
+
+//fs.writeFileSync("./data1.txt","Hello , this is my new work", "utf8");
+//fs.writeFileSync("./data1.txt","Abes", "utf8");
+//fs.appendFileSync("./data1.txt","Engineering college", "utf8");
+//fs.renameSync("./data2.txt","./data3.txt");
